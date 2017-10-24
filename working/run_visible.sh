35ebd7b904e742b8a781644a3455f6a8f3ea58c6
YutaroKikuchi <yusan1871@gmail.com>
2017-10-24 14:49:04 +0900

    Add useful shellfile to build/run docker image/container

---
+++ working/run_visible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo usage : 1 parameter is required
+  exit 1
+fi
+
+DIRECTORY=`basename $1`
+
+docker run -ti --net=host -e DISPLAY=${DISPLAY} --rm --name ${DIRECTORY} ${DIRECTORY}

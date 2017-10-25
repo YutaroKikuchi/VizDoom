d089c0970bc175b802b065dad8b6870fc9249f43
YutaroKikuchi <yusan1871@gmail.com>
2017-10-25 13:57:54 +0900

    Improve vision_partition.py

--- working/vision_partition/vision_partition.py
+++ working/vision_partition/vision_partition.py
@@ -15,37 +15,37 @@ class VP_Network(object):
 
         self.testdata = [[10 for i in range(120)] for j in range(120)]
 
-        self.s1_ = tf.plaeceholder(tf.float32,[None] + list(resolution) + [1],name="state")
+        self.s1_ = tf.placeholder(tf.float32,[None] + list(resolution)+[1],name="state")
         #self.a_ = tf.placeholder(tf.int32, [None],name="partition")
 
-        self.conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=60, kernel_size=[3,3],stride[1,1],
+        self.conv1 = tf.contrib.layers.convolution2d(self.s1_, num_outputs=60, kernel_size=[3,3],stride=[1,1],
         activation_fn=tf.nn.relu,
         weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
         biases_initializer=tf.constant_initializer(0.1))
         
-        self.max_pool1 = tf.contrib.layers.max_pool2d(conv1,kernel_size=[2,2])
+        self.max_pool1 = tf.contrib.layers.max_pool2d(self.conv1,kernel_size=[2,2])
         
-        self.conv2 = tf.contrib.layers.convolution2d(max_pool1, num_outputs=60, kernel_size=[3,3],stride[1,1],
+        self.conv2 = tf.contrib.layers.convolution2d(self.max_pool1, num_outputs=60, kernel_size=[3,3],stride=[1,1],
         activation_fn=tf.nn.relu,
         weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
         biases_initializer=tf.constant_initializer(0.1))
         
-        self.max_pool2 = tf.contrib.layers.max_pool2d(conv2,kernel_size=[2,2])
+        self.max_pool2 = tf.contrib.layers.max_pool2d(self.conv2,kernel_size=[2,2])
 
-        self.fc1 = tf.contrib.layer.fully_connected(max_pool2,num_outputs=40,activation_fn=tf..nn.relu,
+        self.fc1 = tf.contrib.layers.fully_connected(self.max_pool2,num_outputs=40,activation_fn=tf.nn.relu,
         weights_initializer=tf.contrib.layers.xavier_initializer(),
         biases_initializer=tf.constant_initializer(0.1))
 
-        self.fc2 = tf.contrib.layer.fully_connected(fc1,num_outputs=5,activation_fn=tf..nn.relu,
+        self.fc2 = tf.contrib.layers.fully_connected(self.fc1,num_outputs=5,activation_fn=tf.nn.relu,
         weights_initializer=tf.contrib.layers.xavier_initializer(),
         biases_initializer=tf.constant_initializer(0.1))
 
-        self.output = tf.contrib.layer.softmax(fc2)
+        self.output = tf.contrib.layers.softmax(self.fc2)
 
     def test(self):
-        sess = tf.sess
+        sess = tf.Session()
 
-        print(sess.run(self.output,{s1_: self.testdata}))
+        print(sess.run(self.output,feed_dict={self.s1_: [self.testdata]}))
 
 
 network = VP_Network()

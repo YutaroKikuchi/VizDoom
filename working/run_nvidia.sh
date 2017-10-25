56034c4ecc8b07aeba98d842ac78c0aa3657a54f
YutaroKikuchi <yusan1871@gmail.com>
2017-10-25 15:37:08 +0900

    Add run_nvidia.sh Update Dockerfile tensorflow-gpu is activated

---
+++ working/run_nvidia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo usage : 1 parameter is required
+  exit 1
+fi
+
+DIRECTORY=`basename $1`
+
+nvidia-docker run -ti --net=host --privileged -e DISPLAY=${DISPLAY} --rm --name ${DIRECTORY} ${DIRECTORY}

efc2a7d5b766b9fdfc38008f16572dc6fc27f57b
vizdoom <louis.arbaretier@gmail.com>
2017-11-14 10:40:38 +0900

    test with chainerrl training function

--- working/chainer_test/chaintest4.py
+++ working/chainer_test/chaintest4.py
@@ -18,7 +18,7 @@ import numpy as np
 from skimage.color import rgb2gray
 from skimage.transform import resize
 
-n_episodes = 20
+n_episodes = 220
 save_every = 5
 
 
@@ -60,15 +60,19 @@ class DOOM_ENV(env.Env):
         self.frame = 0
         self.legal_actions = [[True, False, False], [False, True, False], [False, False, True]]
         self.episode_len = episode_len
-        self.current_screen = self.game.get_state().screen_buffer
+        obs = resize(rgb2gray(self.game.get_state().screen_buffer), (80, 80))
+        obs = obs[np.newaxis, :, :]
+        self.current_screen = obs
 
     @property
     def state(self):
         if self.game.is_episode_finished():
             return self.current_screen
         rr = self.game.get_state()
-        self.current_screen = rr.screen_buffer
-        return rr.screen_buffer
+        obs = resize(rgb2gray(rr.screen_buffer), (80, 80))
+        obs = obs[np.newaxis, :, :]
+        self.current_screen = obs
+        return obs
     
     def random_action_doom(self,nothing=0) :
         result = choice(range(0,len(self.legal_actions)))
@@ -159,6 +163,87 @@ agent = chainerrl.agents.DoubleDQN(
 last_time = datetime.datetime.now()
 
 filename = "toreplace"
+
+chainerrl.experiments.train_agent_with_evaluation(
+    agent, env,
+    steps=2000,           # Train the agent for 2000 steps
+    eval_n_runs=10,       # 10 episodes are sampled for each evaluation
+    max_episode_len=200,  # Maximum length of each episodes
+    eval_interval=1000,   # Evaluate the agent after every 1000 steps
+    outdir='result')      # Save everything to 'result' directory
+
+print("demo starts")
+#agent.load(filename)
+
+for i in range(1, n_episodes + 1):
+    obs = resize(rgb2gray(env.reset()),(80,80))
+    obs = obs[np.newaxis, :, :]
+
+    reward = 0
+    done = False
+    R = 0
+
+    while not done:
+        action = agent.act(obs)
+        #action = agent.act_and_train(obs, reward)
+        #action = agent.act(obs)
+        obs, reward, done, _ = env.step(action)
+        obs = resize(rgb2gray(obs), (80, 80))
+        obs = obs[np.newaxis, :, :]
+
+
+    agent.stop_episode()
+print("demo ended")
+
+"""
+env = DOOM_ENV()
+obs = env.reset()
+
+class QFunction(chainer.Chain):
+    def __init__(self, n_history=1, n_action=6):
+        super().__init__(
+            l1=L.Convolution2D(n_history, 32, ksize=8, stride=4, nobias=False),
+            l2=L.Convolution2D(32, 64, ksize=3, stride=2, nobias=False),
+            l3=L.Convolution2D(64, 64, ksize=3, stride=1, nobias=False),
+            l4=L.Linear(3136, 512),
+            out=L.Linear(512, n_action, initialW=np.zeros((n_action, 512), dtype=np.float32))
+        )
+
+    def __call__(self, x, test=False):
+        s = chainer.Variable(x)
+        h1 = F.relu(self.l1(s))
+        h2 = F.relu(self.l2(h1))
+        h3 = F.relu(self.l3(h2))
+        h4 = F.relu(self.l4(h3))
+        h5 = self.out(h4)
+        return chainerrl.action_value.DiscreteActionValue(h5)
+
+n_action = env.number_of_actions
+print("n action size obs")
+print(n_action)
+n_history=1
+q_func = QFunction(n_history, n_action)
+
+optimizer = chainer.optimizers.Adam(eps=1e-2)
+optimizer.setup(q_func)
+
+gamma = 0.95
+
+explorer = chainerrl.explorers.ConstantEpsilonGreedy(
+    epsilon=0.3, random_action_func=env.random_action_doom)
+
+replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 4)
+
+phi = lambda x: x.astype(np.float32, copy=False)
+
+agent = chainerrl.agents.DoubleDQN(
+    q_func, optimizer, replay_buffer, gamma, explorer,
+    minibatch_size=4, replay_start_size=500,
+     phi=phi)
+
+last_time = datetime.datetime.now()
+
+filename = "toreplace"
 for i in range(1, n_episodes + 1):
     obs = resize(rgb2gray(env.reset()),(80,80))
     obs = obs[np.newaxis, :, :]
@@ -201,7 +286,7 @@ for i in range(1, n_episodes + 1):
     R = 0
 
     while not done:
-        action = agent.act_and_train(obs, 0)
+        action = agent.act(obs)
         #action = agent.act_and_train(obs, reward)
         #action = agent.act(obs)
         obs, reward, done, _ = env.step(action)
@@ -211,3 +296,4 @@ for i in range(1, n_episodes + 1):
 
     agent.stop_episode()
 print("demo ended")
+"""

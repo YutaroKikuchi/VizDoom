51795c14f194392fc5c36f2571f7984742dbf4cd
vizdoom <louis.arbaretier@gmail.com>
2017-11-17 12:01:02 +0900

    better training presentation

--- working/chainer_test/chaintest4.py
+++ working/chainer_test/chaintest4.py
@@ -14,11 +14,16 @@ from chainerrl import env
 from chainerrl import spaces
 import gym
 import numpy as np
+import math
+
+from tqdm import trange
 
 from skimage.color import rgb2gray
 from skimage.transform import resize
 
-n_episodes = 220
+n_epochs = 4
+epoch_len = 500
+test_epoch_len =8
 save_every = 5
 
 
@@ -59,6 +64,15 @@ class DOOM_ENV(env.Env):
         self._reward=0
         self.frame = 0
         self.legal_actions = [[True, False, False], [False, True, False], [False, False, True]]
+        
+        self.action_space = spaces.Discrete(len(self.legal_actions))
+        
+        one_screen_observation_space = spaces.Box(
+            low=0, high=255, shape=(80, 80))
+        n_last_screens = 4
+        self.observation_space = spaces.Tuple(
+            [one_screen_observation_space] * n_last_screens)
+        
         self.episode_len = episode_len
         obs = resize(rgb2gray(self.game.get_state().screen_buffer), (80, 80))
         obs = obs[np.newaxis, :, :]
@@ -73,7 +87,7 @@ class DOOM_ENV(env.Env):
         obs = obs[np.newaxis, :, :]
         self.current_screen = obs
         return obs
-    
+
     def random_action_doom(self,nothing=0) :
         result = choice(range(0,len(self.legal_actions)))
         return result
@@ -104,6 +118,16 @@ class DOOM_ENV(env.Env):
     def reset(self):
         self.initialize()
         return self.state
+    
+    def set_window(self, on):
+        self.game.close()
+        self.game.set_window_visible(on)
+        self.game.init()
+        self.game.new_episode()
+        return on
+    
+    def get_total_score(self):
+        return self.game.get_total_reward()
 
     def step(self, action):
         self.frame = self.frame + 1
@@ -115,11 +139,12 @@ class DOOM_ENV(env.Env):
 
 
 #---------------------------------------------------------------------------
+
 env = DOOM_ENV()
 obs = env.reset()
 
 class QFunction(chainer.Chain):
-    def __init__(self, n_history=1, n_action=6):
+    def __init__(self, n_history=1, n_action=3):
         super().__init__(
             l1=L.Convolution2D(n_history, 32, ksize=8, stride=4, nobias=False),
             l2=L.Convolution2D(32, 64, ksize=3, stride=2, nobias=False),
@@ -143,7 +168,7 @@ print(n_action)
 n_history=1
 q_func = QFunction(n_history, n_action)
 
-optimizer = chainer.optimizers.Adam(eps=1e-2)
+optimizer = chainer.optimizers.Adam(eps=1e-2)#1e-2
 optimizer.setup(q_func)
 
 gamma = 0.95
@@ -151,33 +176,23 @@ gamma = 0.95
 explorer = chainerrl.explorers.ConstantEpsilonGreedy(
     epsilon=0.3, random_action_func=env.random_action_doom)
 
-replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 4)
+replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 11)
 
 phi = lambda x: x.astype(np.float32, copy=False)
 
 agent = chainerrl.agents.DoubleDQN(
     q_func, optimizer, replay_buffer, gamma, explorer,
-    minibatch_size=4, replay_start_size=500,
+    minibatch_size=4, n_times_update=1, replay_start_size=50, target_update_interval=100,
      phi=phi)
 
-last_time = datetime.datetime.now()
 
-filename = "toreplace"
+env.set_window(True)
 
-chainerrl.experiments.train_agent_with_evaluation(
-    agent, env,
-    steps=2000,           # Train the agent for 2000 steps
-    eval_n_runs=10,       # 10 episodes are sampled for each evaluation
-    max_episode_len=200,  # Maximum length of each episodes
-    eval_interval=1000,   # Evaluate the agent after every 1000 steps
-    outdir='result')      # Save everything to 'result' directory
 
-print("demo starts")
-#agent.load(filename)
-
-for i in range(1, n_episodes + 1):
-    obs = resize(rgb2gray(env.reset()),(80,80))
-    obs = obs[np.newaxis, :, :]
+for i in range(1, 1 + 1):
+    obs = env.reset()
+    #obs = resize(rgb2gray(env.reset()),(80,80))
+    #obs = obs[np.newaxis, :, :]
 
     reward = 0
     done = False
@@ -188,81 +203,97 @@ for i in range(1, n_episodes + 1):
         #action = agent.act_and_train(obs, reward)
         #action = agent.act(obs)
         obs, reward, done, _ = env.step(action)
-        obs = resize(rgb2gray(obs), (80, 80))
-        obs = obs[np.newaxis, :, :]
+        #obs = resize(rgb2gray(obs), (80, 80))
+        #obs = obs[np.newaxis, :, :]
 
 
     agent.stop_episode()
-print("demo ended")
 
-"""
-env = DOOM_ENV()
-obs = env.reset()
-
-class QFunction(chainer.Chain):
-    def __init__(self, n_history=1, n_action=6):
-        super().__init__(
-            l1=L.Convolution2D(n_history, 32, ksize=8, stride=4, nobias=False),
-            l2=L.Convolution2D(32, 64, ksize=3, stride=2, nobias=False),
-            l3=L.Convolution2D(64, 64, ksize=3, stride=1, nobias=False),
-            l4=L.Linear(3136, 512),
-            out=L.Linear(512, n_action, initialW=np.zeros((n_action, 512), dtype=np.float32))
-        )
-
-    def __call__(self, x, test=False):
-        s = chainer.Variable(x)
-        h1 = F.relu(self.l1(s))
-        h2 = F.relu(self.l2(h1))
-        h3 = F.relu(self.l3(h2))
-        h4 = F.relu(self.l4(h3))
-        h5 = self.out(h4)
-        return chainerrl.action_value.DiscreteActionValue(h5)
-
-n_action = env.number_of_actions
-print("n action size obs")
-print(n_action)
-n_history=1
-q_func = QFunction(n_history, n_action)
-
-optimizer = chainer.optimizers.Adam(eps=1e-2)
-optimizer.setup(q_func)
-
-gamma = 0.95
-
-explorer = chainerrl.explorers.ConstantEpsilonGreedy(
-    epsilon=0.3, random_action_func=env.random_action_doom)
-
-replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10 ** 4)
-
-phi = lambda x: x.astype(np.float32, copy=False)
-
-agent = chainerrl.agents.DoubleDQN(
-    q_func, optimizer, replay_buffer, gamma, explorer,
-    minibatch_size=4, replay_start_size=500,
-     phi=phi)
 
 last_time = datetime.datetime.now()
 
 filename = "toreplace"
-for i in range(1, n_episodes + 1):
-    obs = resize(rgb2gray(env.reset()),(80,80))
-    obs = obs[np.newaxis, :, :]
+env.set_window(False)
 
+
+print("Starting the training!")
+
+for epoch in range(n_epochs):
+    print("\nEpoch %d\n-------" % (epoch + 1))
+    train_episodes_finished = 0
+    train_scores = []
+    reward = 0
+    done = False
+
+    print("Training...")
+    obs = env.reset()
+    for learning_step in trange(epoch_len, leave=False):
+        action = agent.act_and_train(obs, reward)
+        obs, reward, done, _ = env.step(action)
+        if done:
+            score = env.get_total_score()
+            train_scores.append(score)
+            obs = env.reset()
+            train_episodes_finished += 1
+
+    print("%d training episodes played." % train_episodes_finished)
+
+    train_scores = np.array(train_scores)
+    agent.stop_episode_and_train(obs, score, done)
+    print("Results: mean :",train_scores.mean()," plusminus " ,train_scores.std(), "min: ", train_scores.min(), "max: ", train_scores.max())
+
+    print("\nTesting...")
+    test_episode = []
+    test_scores = []
+    for test_episode in trange(test_epoch_len, leave=False):
+        done = False
+        obs = env.reset()
+        while not done:
+            action = agent.act(obs)
+            obs, reward, done, _ = env.step(action)
+        r = env.get_total_score()
+        test_scores.append(r)
+        agent.stop_episode()
+
+    test_scores = np.array(test_scores)
+    print("Results: mean :",test_scores.mean()," plusminus " ,test_scores.std(), "min: ", test_scores.min(), "max: ", test_scores.max())
+
+    #print("Saving the network weigths to:", model_savefile)
+    #saver.save(session, model_savefile)
+
+    print("Total elapsed time: ", ((datetime.datetime.now() - last_time) / 60.0), " minutes")
+
+
+print("======================================")
+print("Training finished. It's time to watch!")
+
+
+"""
+
+
+for i in range(1, n_epochs + 1):
+    obs = env.reset()
+    #obs = resize(rgb2gray(env.reset()),(80,80))
+    #obs = obs[np.newaxis, :, :]
+    t = 0
     reward = 0
     done = False
     R = 0
 
-    while not done:
+    while t<epoch_len:
         action = agent.act_and_train(obs, reward)
         obs, reward, done, _ = env.step(action)
-        obs = resize(rgb2gray(obs), (80, 80))
-        obs = obs[np.newaxis, :, :]
-
+        #obs = resize(rgb2gray(obs), (80, 80))
+        #obs = obs[np.newaxis, :, :]
+        t = t + 1
         if reward != 0:
             R += reward
+        if done:
+            obs = env.reset()
+            done=False
 
     elapsed_time = datetime.datetime.now() - last_time
-    print('episode:', i, '/', n_episodes,
+    print('epoch:', i, '/', n_epochs,
           'reward:', R,
           'minutes:', elapsed_time.seconds/60)
     last_time = datetime.datetime.now()
@@ -276,10 +307,15 @@ print('Finished. Now testing')
 
 print("demo starts")
 #agent.load(filename)
+"""
 
-for i in range(1, n_episodes + 1):
-    obs = resize(rgb2gray(env.reset()),(80,80))
-    obs = obs[np.newaxis, :, :]
+
+env.set_window(True)
+
+for i in range(1, 300 + 1):
+    obs = env.reset()
+    #obs = resize(rgb2gray(env.reset()),(80,80))
+    #obs = obs[np.newaxis, :, :]
 
     reward = 0
     done = False
@@ -290,11 +326,11 @@ for i in range(1, n_episodes + 1):
         #action = agent.act_and_train(obs, reward)
         #action = agent.act(obs)
         obs, reward, done, _ = env.step(action)
-        obs = resize(rgb2gray(obs), (80, 80))
-        obs = obs[np.newaxis, :, :]
+        #obs = resize(rgb2gray(obs), (80, 80))
+        #obs = obs[np.newaxis, :, :]
 
 
     agent.stop_episode()
 print("demo ended")
-"""
+
 
